2727a60787ca1c8e4eeb922a1570e904a6d9aa96
carefreecaribou <55297934+carefreecaribou@users.noreply.github.com>
2023-09-28 18:29:41 -0400

    Push Release 1.0
    Signed-off-by: carefreecaribou <55297934+carefreecaribou@users.noreply.github.com>

---
+++ .autoprovision.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ -d ~/Development/ansible-pop_os/ ]; 
+then
+    echo "Playbook is already local. Cannot safely autoprovision. Delete ~/Development/ansible-pop_os and rerun, or manually run the playbook to continue.";
+    exit 1;
+fi
+
+# Update Packages
+sudo apt update && sudo NEEDRESTART_MODE=a apt-get -y upgrade
+sudo apt install -y git
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py --user
+python3 -m pip install --user ansible jinja2
+
+# Make Development Folder and Clone the Repo
+mkdir -p ~/Development/ansible-pop_os2204;
+git clone https://github.com/carefreecaribou/ansible-pop_os2204 ~/Development/ansible-pop_os2204;
+[ -f ~/provision-config.yml ] && mv ~/provision-config.yml ~/Development/ansible-pop_os2204/config.yml;
+cd ~/Development/ansible-pop_os2204
+
+# Run the Playbook
+ansible-galaxy install -r requirements.yml
+echo "remove_autostart: true" >> config.yml
+echo 'gnome-terminal -- bash -c "cd ~/Development/ansible-pop_os2204 && ansible-playbook main.yml --ask-become-pass; bash"' >> ~/.profile
+sudo systemctl disable pop-upgrade.service
+sudo reboot now
